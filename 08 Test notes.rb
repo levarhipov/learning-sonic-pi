0dc50694e0325ac283ebb111731b15aec1a5b5d7
levarhipov <88651448+levarhipov@users.noreply.github.com>
2021-08-16 10:23:58 +0300

    Add files via upload

---
+++ 08 Test notes.rb
@@ -0,0 +1,53 @@
+#TEST
+
+# Используемые и Неиспользуемые ноты, от 60 до 83
+
+# Use and Un-use notes, 60 - 83
+play :C4 # Do 60
+sleep 1
+play note 61
+sleep 1
+play :D4 # Re 62
+sleep 1
+play note 63
+sleep 1
+play :E4 # Mi 64
+sleep 1
+play :F4 # Fa 65
+sleep 1
+play note 66
+sleep 1
+play :G4 # Sol 67
+sleep 1
+play note 68
+sleep 1
+play :A4 # Lja 69
+sleep 1
+play note 70
+sleep 1
+play :B4 # Si 71
+sleep 1
+play :C5 # Do Second 72
+sleep 1
+play note 73
+sleep 1
+play :D5 # Re Second 74
+sleep 1
+play note 75
+sleep 1
+play :E5 # Mi Second 76
+sleep 1
+play :F5 # Fa Second 77
+sleep 1
+play note 78
+sleep 1
+play :G5 # Sol Second 79
+sleep 1
+play note 80
+sleep 1
+play :A5 # Lja Second 81
+sleep 1
+play note 82
+sleep 1
+play :B5 # Si Second 83
+sleep 1
